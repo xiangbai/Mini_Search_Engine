2a822419a0e8145d4e434c4f8acf7fc5dc998d32
xiangbai <wanglei8568@126.com>
2014-05-19 17:18:38 +0800

    倒排索引类

---
+++ include/Inverted.h
@@ -0,0 +1,17 @@
+/*
+ * Inverted.h
+ *
+ *  Created on: May 19, 2014
+ *      Author: wang
+ */
+
+#ifndef INVERTED_H_
+#define INVERTED_H_
+
+class Inverted {
+public:
+	Inverted();
+	virtual ~Inverted();
+};
+
+#endif /* INVERTED_H_ */
---
+++ src/Inverted.cpp
@@ -0,0 +1,18 @@
+/*
+ * Inverted.cpp
+ *
+ *  Created on: May 19, 2014
+ *      Author: wang
+ */
+
+#include "Inverted.h"
+
+Inverted::Inverted() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Inverted::~Inverted() {
+	// TODO Auto-generated destructor stub
+}
+
