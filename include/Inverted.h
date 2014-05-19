7d83d29447321ef4a10bdce9c67e1682bb3bce1c
xiangbai <wanglei8568@126.com>
2014-05-19 22:47:19 +0800

    建立倒排索引

--- include/Inverted.h
+++ include/Inverted.h
@@ -4,14 +4,30 @@
  *  Created on: May 19, 2014
  *      Author: wang
  */
-
+/*
+ * 建立倒排索引表
+ */
 #ifndef INVERTED_H_
 #define INVERTED_H_
-
+#include <string>
+#include <fstream>
+#include <map>
+#include <utility>
+#include <vector>
+#include <stdlib.h>
+#include <stdio.h>
+class Document ;
 class Inverted {
 public:
-	Inverted();
+	Inverted(const std::string &invertedfile,Document &doc);  //存储倒排索引中的内容
 	virtual ~Inverted();
+	void read_map_to_file(const std::string &invertedfile);
+private:
+	/*
+	 * 如何解析出这样的关联数组
+	 */
+	std::map<std::string, std::vector<std::pair<int, int> > >m_invert ;  //关键词，文章编号，词频
+	Document &doc;
 };
 
 #endif /* INVERTED_H_ */
--- src/Inverted.cpp
+++ src/Inverted.cpp
@@ -6,12 +6,49 @@
  */
 
 #include "Inverted.h"
-
-Inverted::Inverted() {
+#include "Document.h"
+#include "WebPage.h"
+Inverted::Inverted(const std::string &invertedfile,Document &doc):doc(doc){
 	// TODO Auto-generated constructor stub
-
+	m_invert.clear();
+	read_map_to_file(invertedfile);
 }
-
+void Inverted::read_map_to_file(const std::string &invertedfile)
+{
+	std::ofstream fout;
+	fout.open(invertedfile.c_str());
+	if(!fout)
+	{
+		perror("file open failed");
+		exit(1);
+	}
+	//不含重复网页
+	//关键字，文章号， 词频
+//	EncodingConverter trans ;
+//	std::string str ;
+	for(std::vector<WebPage>::size_type ix = 0; ix != doc.v_webpage.size(); ++ix)
+	{
+		for(std::map<std::string, int>::iterator iter = doc.v_webpage[ix].m_word.begin(); iter != doc.v_webpage[ix].m_word.end(); ++iter)
+		{
+			//str = iter->first;
+			//std::cout<<"str: "<<trans.gbkToutf8(str)<<std::endl;
+			m_invert[iter->first].push_back(std::make_pair( doc.v_webpage[ix]._docid,iter->second));
+		}
+	}
+	std::cout<<"m_invert size: "<<m_invert.size()<<std::endl;
+	std::map<std::string, std::vector<std::pair<int, int> > >::iterator iter = m_invert.begin() ;
+	while(iter != m_invert.end())
+	{
+		fout<<iter->first<<"   ";
+		for(std::vector<std::pair<int, int> >::iterator v_iter = iter->second.begin(); v_iter != iter->second.end(); ++v_iter)
+		{
+			fout<<v_iter->first<<"   "<<v_iter->second<<"   ";
+		}
+		fout<<std::endl;
+		++iter;
+	}
+	fout.close();
+}
 Inverted::~Inverted() {
 	// TODO Auto-generated destructor stub
 }
