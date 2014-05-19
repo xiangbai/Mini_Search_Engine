ee8ee334f90e51cb2e9f601b179e66b51b8a4192
xiangbai <wanglei8568@126.com>
2014-05-19 17:21:48 +0800

    更好的封装分词程序，运行前，加载相应文件到内存中

--- include/Document.h
+++ include/Document.h
@@ -27,25 +27,27 @@
 #include "EncodingConverter.h"
 #include "WebPage.h"
 #include "Conf.h"
+
 /*
  * Document是为每一个网页进行分词操作
  */
+class Offset;
+class Segment;
 class Document {
 friend class UniquePage ;
 public:
-	Document(const std::string &webpage, const std::string &indexpage, const std::string &stopfile);
+	//Document(const std::string &webpage, const std::string &indexpage, const std::string &stopfile);
+	Document(Offset &offset, Segment &seg, const std::string &topfile);
 	virtual ~Document();
-	void read_page(const std::string &webpage, const std::string &indexpage);
 	void get_info(WebPage &page , std::string &document);  //获取网页的content内容
 	void display(int docid, WebPage &page);
+	void deal_page();
+	void write_top_word();
 private:
-
+	Offset &offset ;
+	Segment &seg ;
 	std::vector<WebPage> v_webpage ;
-	std::string _stopfile ;
-	std::priority_queue<std::pair< int,std::string >, std::vector<std::pair<int, std::string > >,std::less<std::pair< int,std::string> > > queueKless;
-
-	void sort_page_words(std::map<std::string, int> &m_word, int topN);  //排序，获取TOPN的词
-	void one_page_info(const std::string &str, FILE *p_page, FILE*p_index);
+	std::ofstream _fout;
 
 };
 
--- src/Document.cpp
+++ src/Document.cpp
@@ -12,77 +12,48 @@
 #include <ctype.h>
 #include <stdint.h>
 #include "UniquePage.h"
+#include "Offset.h"
 #include "Segment.h"
-
+#include "MixSegment.hpp"
 #define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
 #define TOPN 10
 using namespace CppJieba;
 
-Document::Document(const std::string &webpage, const std::string &indexpage, const std::string &stopfile)
-	:v_webpage(0), _stopfile(stopfile){        //初始化列表
-	// TODO Auto-generated constructor stub
-	read_page(webpage, indexpage);
-}
-
-//读取配置文件中每个网页文件
-void Document::read_page(const std::string &webpage, const std::string &indexpage)
+Document::Document(Offset &offset, Segment &seg, const std::string &topfile):offset(offset), seg(seg), v_webpage(0)
 {
-	FILE *p_page, *p_index ;
-	p_page = fopen(webpage.c_str(), "r");  //读网页库文件
-	p_index = fopen(indexpage.c_str(), "r");   //读取索引文件
-	if(p_page == NULL || p_index == NULL)
-	{
-		perror("Error opening file");
-	}
-	int start_index ;
-	int end_index ;
-	char str[100] ;
-	memset(str, 0, 100);
-	std::cout<<"before the size is: "<<v_webpage.size()<<std::endl;
-	while(fgets(str, 100, p_index) != NULL)   //顺序读取索引值
-	{
-		one_page_info(str, p_page, p_index);
-	}
-
-	std::cout<<"v_webpage the size is: "<<v_webpage.size()<<std::endl;
-	fclose(p_page);
-	fclose(p_index);
+	deal_page();
 }
-void Document::one_page_info(const std::string &str, FILE *p_page, FILE*p_index)
+void Document::deal_page()
 {
-	int start_index ;
-	int end_index ;
-	WebPage page ;
-	sscanf(str.c_str(), "%d%d%d", &page._docid, &start_index, &end_index);  //得到文档的开始和结束部分
-	fseek(p_page, start_index, SEEK_SET); //定位文件指针
-
-	char *result = new char[end_index + 1]() ;
-	fgets(result, end_index + 1, p_page) ; //获取一个文档的内容
-
-	std::string document(result) ;
-	get_info(page,document);  //分别获取文档的content内容
-
-	Segment seg(_stopfile);
-	seg.cut_page(page._content, page.m_word);
-
-	//构建优先级对列
-	for(std::map<std::string, int>::iterator iter = page.m_word.begin(); iter != page.m_word.end(); ++iter)
+	for(std::map<int, std::string>::iterator iter = offset.v_content.begin(); iter != offset.v_content.end(); ++iter)
 	{
-		queueKless.push(std::make_pair(iter->second, iter->first));   //去停用词之后的按词频的排序
-	}
-	/*
-	* 分完词之后求词频的topN，然后将其加入到vector中
-	*/
-	sort_page_words(page.top_word,TOPN);  //获取词和词频的topN
-	/*
-	* 可以使用优先级队列来进行排序，对map对象中的元素按照词频进行排序
-	* 需要考虑所要使用的排序函数，如优先级队列
-	*/
-	v_webpage.push_back(page) ;     //将排序之后的网页库加入到数组中，以备后面的网页去重使用
-	delete [] result ;
-	std::cout<<"over page "<<page._docid<<std::endl;
-}
+		WebPage page ;
+		get_info(page, iter->second); //获取content
+		page._docid = iter->first ;
+		seg.cut_page(page._content, page.m_word);//分词
 
+		//构建优先级对列
+		std::priority_queue<std::pair< int,std::string >, std::vector<std::pair<int, std::string > >,std::less<std::pair< int,std::string> > > queueKless;
+		for(std::map<std::string, int>::iterator iter = page.m_word.begin(); iter != page.m_word.end(); ++iter)
+		{
+			queueKless.push(std::make_pair(iter->second, iter->first));   //去停用词之后的按词频的排序
+		}
+		int topN = MIN(TOPN, queueKless.size());
+	//	_fout<<page._docid<<"\t";
+		for(int i = 0 ; i < topN; ++i)
+		{
+			page.top_word.push_back(queueKless.top().second) ;   //对应的docid所对应的topn词频
+		//	_fout<<queueKless.top().second<<"\t" ;
+			queueKless.pop() ;
+		}
+	//	_fout<<std::endl;
+		v_webpage.push_back(page) ;     //将排序之后的网页库加入到数组中，以备后面的网页去重使用
+		if(page._docid %1000 ==0)
+		{
+			std::cout<<"ok"<<std::endl;
+		}
+	}
+}
 void Document::display(int docid, WebPage &page)
 {
 	EncodingConverter trans;
@@ -90,11 +61,11 @@ void Document::display(int docid, WebPage &page)
 	{
 		std::cout<<"page.m_word: "<<page.m_word.size()<<std::endl;
 		std::cout<<"page.top_word: "<<page.top_word.size()<<std::endl;
-		for(std::map<std::string, int>::iterator iter = page.top_word.begin(); iter != page.top_word.end(); ++iter)
+		for(std::vector<std::string>::iterator iter = page.top_word.begin(); iter != page.top_word.end(); ++iter)
 		{
-			std::string temp = iter->first;
+			std::string temp = *iter;
 			temp = trans.gbkToutf8(temp);
-			std::cout<<temp<<"-------"<<iter->second<<std::endl;
+			std::cout<<temp<<"---"<<std::endl;
 		}
 		std::cout<<"*************"<<std::endl;
 	}
@@ -123,18 +94,7 @@ void Document::get_info(WebPage &page , std::string &document)
 	page._content = document.substr(content_begin+9, content_end-content_begin-9) ;
 }
 
-//使用优先级队列，以获取TOPN的词组
-void Document::sort_page_words(std::map<std::string, int> &top_word, int topN)
-{
-	topN = MIN(topN, queueKless.size());
-	 pair<int,string>tmp;
-	for(int i = 0 ; i < topN; ++i)
-	{
-		tmp = queueKless.top() ;
-		top_word.insert(std::make_pair(tmp.second, tmp.first)) ;
-		queueKless.pop() ;
-	}
-}
+
 Document::~Document() {
 	// TODO Auto-generated destructor stub
 }
@@ -143,8 +103,16 @@ Document::~Document() {
 int main(int argc, char **argv)
 {
 	Conf conf(argv[1]);
-	Document doc(conf.get_value("Web_page"), conf.get_value("Index_page"), conf.get_value("Stop_list"));
-	//UniquePage uqpage(doc);
-	std::cout<<"end"<<std::endl;
+
+	std::string dict = conf.get_value("dict_path") ;
+	std::string model = conf.get_value("model_path") ;
+	MixSegment segment(dict.c_str(), model.c_str()) ;
+	Offset offset(conf.get_value("Web_page"), conf.get_value("Index_page"));
+	Segment seg(conf.get_value("Stop_list"), segment);
+
+	Document doc(offset, seg,conf.get_value("TopN_list"));
+	std::cout<<"end document"<<std::endl;
+	UniquePage uqpage(conf.get_value("New_index"),doc, offset);
+	std::cout<<"end unique"<<std::endl;
 	return 0 ;
 }
